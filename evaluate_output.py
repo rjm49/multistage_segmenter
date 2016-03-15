f5e86dcc6277542affc7a46d24844b92f7b5050b
Russell Moore <russell.moore@greateastlondon.com>
2016-03-15 21:02:47 +0000

    minor update to evaluate_output.py

--- evaluate_output.py
+++ evaluate_output.py
@@ -18,6 +18,53 @@ def open_wfile(odir,transcript_id):
     fhandle = codecs.open(ofilename, 'w')
     return fhandle
 
+#this lets us compare the contents of two files
+def compare_rows(gold_rows, algo_rows):
+    fps=tps=fns=tns = 0.0 #reset our counters for each record, as floats
+    nobrks_out=nobrks_gold = 0
+
+    while gold_rows != []:
+        gold_row = gold_rows.pop(0)
+        out_row = output_rows.pop(0)
+        #print gold_row,"vs",out_row
+        
+        if(gold_row == out_row):
+            if gold_row == BREAK:
+                nobrks_gold += 1
+                nobrks_out += 1
+                tps += 1
+            else:
+                tns += 1
+        else: #either outrow has missed a break, or added one too many
+            if gold_row == BREAK: #out missed a break
+                nobrks_gold+=1
+                fns += 1
+                gold_row = gold_rows.pop(0)
+            else:
+                nobrks_out+=1
+                fps += 1
+                out_row = output_rows.pop(0)
+    
+    remaining = len(output_rows)
+    if(remaining)>0:
+        print "Dangling output row remaining=", remaining
+        print output_rows
+        fps+=1
+    
+    #print tps, tns
+    #print fps, fns
+    p = 1.0 if (tps+fps==0) else (tps / (tps + fps)) #cases found / (cases found + wrongly assumed cases)
+    r = 1.0 if (tps+fns==0) else (tps / (tps + fns)) #cases found / (cases found + cases not found)
+     
+    #print p, r
+    F = 0 if (p+r==0) else (2*p*r / (p+r))
+    print outf,"- p/r/F:",p,r,F
+
+    recid = os.path.basename(outf)[:-4]
+
+    return (recid, nobrks_gold, nobrks_out, tps, fps, fns, tns, p,r,F)
+
+
 if __name__ == '__main__':
     lmdir = "eval1n"
     
