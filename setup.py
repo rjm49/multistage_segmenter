4a59ad888ab7f23e1d7f301b4c37102660c8fe0c
rjm49 <russell.moore@greateastlondon.com>
2016-04-29 15:47:12 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='multistage_segmenter',
+      version='0.1',
+      author='russell moore',
+      author_email='russell@russellmoo.re',
+      packages=['slm','pm','lm'],
+      scripts=['scripts'],
+)
