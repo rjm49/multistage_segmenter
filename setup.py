9b1b21f0d04d384c7122f7a0f6fe0f33d638b2cd
Russell Moore <russell.moore@greateastlondon.com>
2016-11-16 14:44:14 +0000

    added cleanup command to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import os
 
 setup(name='mseg',
       version='0.16',
@@ -8,3 +9,5 @@ setup(name='mseg',
       packages=find_packages(exclude=('tests','docs')),
       scripts=['scripts/train_pm.py','scripts/main_run.py','scripts/create_models.py'],
 )
+
+os.system('rm -rf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
